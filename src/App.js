7b1ba92ba3753611c10d9dcfc4822d28a08138f1
kmanthrige <k.manthrige@gmail.com>
2023-06-23 07:39:35 -0500

    search engine complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+
+  const [searchPics, setSearchPics] = useState([]);
+  const [userSearched, setUserSearched] = useState("");
+  const [color, setColor] = useState("");
+
+  const searchItem = async () => {
+    try {
+      const apiKey = process.env.REACT_APP_SEARCH_PROJECT
+      const response = await fetch(`https://api.pexels.com/v1/search?query=${userSearched}&color=${color}`, { headers: { Authorization: apiKey } })
+      const data = await response.json();
+      setSearchPics(data.photos)
+    } catch (error) {
+      console.log("Data not found")
+    }
+  }
+
+  const handleSearchBar = (event) => {
+    setUserSearched(event.target.value)
+  }
+
+  const handleColor = (event) => {
+    setColor(event.target.value)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="function">
+      <h1>Image Search</h1>
+      <div className="searchBarBtn">
+      <input className="input" placeholder="Search for an image here" onChange={handleSearchBar}/>
+      <input className="color" placeholder="color" onChange={handleColor}/>
+      <button onClick={searchItem} >Search Pictures</button>
+      </div>
+      <div className="grid">
+        {searchPics.map((item, index) => (
+          <img src={item.src.medium} alt={item.alt} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
